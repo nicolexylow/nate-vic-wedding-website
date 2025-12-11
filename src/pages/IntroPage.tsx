25b4dd3507b378117715e138419092a64763c281
Nicole Low <nicolexylow@gmail.com>
2025-12-11 19:37:26 +1100

    initial gh-pages setup

--- src/pages/IntroPage.tsx
+++
@@ -1,9 +0,0 @@
-import CardImage from "../components/CardImage";
-
-export default function IntroPage() {
-  return (
-    <div className="bg-[#ffedf3] text-[#233235] px-12 flex items-center justify-center">
-      <CardImage />
-    </div>
-  );
-}
